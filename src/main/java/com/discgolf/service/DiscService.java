8ea0e7c81a8747b10481a8a663995384fc8b1c4f
randy <randyn1080@gmail.com>
2025-04-11 18:56:21 -0400

    Created DiscService interface; Updated documentation in DiscDAO;

--- src/main/java/com/discgolf/service/DiscService.java
+++ src/main/java/com/discgolf/service/DiscService.java
@@ -1,54 +1,49 @@
-package com.discgolf.dao;
+package com.discgolf.service;
 
 import com.discgolf.model.Disc;
 import com.discgolf.model.DiscSearchCriteria;
 
 import java.util.List;
 
-public interface DiscDAO {
-    // Create
+public interface DiscService {
     /**
-     * Creates a new disc in the database
-     * @param disc The disc to create
+     * Create a new disc
+     * @param disc Disc to create
      * @return the created disc with its generated ID
      */
     Disc createDisc(Disc disc);
 
-    // Read
     /**
-     * Retrieves all discs from the database
+     * Retrieve all discs
      * @return List of all discs
      */
     List<Disc> getAllDiscs();
 
     /**
-     * Retrieve a disc by its ID
+     * Retrieve a disc by ID
      * @param discId The ID of the disc to retrieve
      * @return the disc if found, null otherwise
      */
     Disc getDiscById(int discId);
 
     /**
-     * Searches for discs based on a set of criteria
+     * Search for discs based on a set of criteria
      * @param criteria The search criteria object containing any combination of filters
      * @return List of discs matching the criteria
      */
     List<Disc> searchDiscs(DiscSearchCriteria criteria);
 
-    // Update
     /**
-     * Updates an existing disc in the database
+     * Update an existing disc
      * @param disc The disc to update
      * @return True if the disc was updated, false otherwise
      */
     boolean updateDisc(Disc disc);
 
-    // Delete
     /**
-     * Deletes a disc from the database
-     * @param discId The ID of the disc to be deleted
-     * @return true if the disc was deleted, false otherwise
+     * Delete a disc by its ID
+     * @param discId ID of disc to be deleted
+     * @return true if disc was deleted, false otherwise
      */
     boolean deleteDisc(int discId);
-
 }
