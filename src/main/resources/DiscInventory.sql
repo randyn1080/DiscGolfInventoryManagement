d86b1e03d2780cfcb4d2ff1868aef6915afd34e2
randy <randyn1080@gmail.com>
2025-04-11 14:27:29 -0400

    Created disc_inventory DB on local MySQL; initialized 3 different discs;

---
+++ src/main/resources/DiscInventory.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE disc_inventory;
+USE disc_inventory;
+
+CREATE TABLE discs (
+   disc_id INT PRIMARY KEY AUTO_INCREMENT,
+   manufacturer VARCHAR(100) NOT NULL,
+   mold VARCHAR(100) NOT NULL,
+   color VARCHAR(100) NOT NULL,
+   weight INT NOT NULL,
+   speed INT NOT NULL,
+   glide INT NOT NULL,
+   turn INT NOT NULL,
+   fade INT NOT NULL,
+   special_notes TEXT
+);
+
+INSERT INTO discs (manufacturer, mold, color, weight, speed, glide, turn, fade, special_notes)
+VALUES ('Discraft', 'Luna', 'blue swirl', 171, 3, 3, 0, 2, 'little luna emblem stamp');
+
+INSERT INTO discs (manufacturer, mold, color, weight, speed, glide, turn, fade, special_notes)
+VALUES ('Innova', 'Destroyer', 'red', 175, 12, 5, -1, 3, 'star plastic');
+
+INSERT INTO discs (manufacturer, mold, color, weight, speed, glide, turn, fade, special_notes)
+VALUES ('MVP', 'Volt', 'green', 168, 8, 5, -1, 2, 'cosmic neutron plastic');
+
